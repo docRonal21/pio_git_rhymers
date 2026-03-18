0892a9a4dcc3728422e0d9d3597bfc5915a2e237
258483 <258483@edu.p.lodz.pl>
2026-03-18 09:13:36 +0100

    6 literaly zastapione deklaracjami stalych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int INT = 12;
+    private int[] numbers = new int[INT];
 
     public int anInt = -1;
 
